05a01b70b0c490bd151e006bf1cc71ebf9068961
Chris Brown <chris@chrisbrown.id.au>
2016-06-01 09:17:48 +1000

    Added sanity check for data age and cleaned up working directory
    * Does not download new imagery if it already has the latest
    * Added capability to move working directory, creates it if it does not
    exist

--- h8-wallpaper.ps1
+++ h8-wallpaper.ps1
@@ -1,20 +1,60 @@
-﻿[void][reflection.assembly]::loadwithpartialname("system.drawing")
+﻿
+######
+# Settings
+######
+$workingDirectory = Join-Path $env:USERPROFILE ".h8-wallpaper"
+
 
 $latestDataUri = "http://himawari8-dl.nict.go.jp/himawari8/img/D531106/latest.json"
 $imageBaseUri = "http://himawari8-dl.nict.go.jp/himawari8/img/D531106/2d/550/"
 
-$wallpaperFile = Join-Path $env:USERPROFILE "h8-wallpaper.png"
+######
+# Script Logic
+######
 
 $h8info = @{
     "XCount" = 2
     "YCount" = 2
+    "LatestStampFile" = Join-Path $workingDirectory "latest.txt"
     }
-    
+
+$AlreadyUpToDate = $false
+
+# Create the working directory if it does not exist
+if (-not (Test-Path $workingDirectory)) {
+    $wd = New-Item $workingDirectory -ItemType Directory
+    $wd.Attributes = "hidden"
+    $wd = $null
+    "Created working directory: '$workingDirectory"
+}
+
+$wallpaperFile = Join-Path $workingDirectory "h8-wallpaper.png"
+
+# Load the System.Drawing namespace so we can do cool image stuff
+[void][reflection.assembly]::loadwithpartialname("system.drawing")
+
 # Grab the metadata file telling us when the latest imagery was uploaded
-$latestData = (Invoke-WebRequest $latestDataUri -ErrorAction Stop).Content | ConvertFrom-Json -ErrorAction Stop
+$latestData = (Invoke-WebRequest $latestDataUri -ErrorAction Stop).Content | 
+                ConvertFrom-Json -ErrorAction Stop
 
 # Build up the file system path for the images
 $latest = get-date $latestData[0].date -f "yyyy/MM/dd/HHmmss"
+
+# Check to see if we already have the latest imagery 
+# (based on timestamp from retrieved latest.json)
+if (Test-Path $h8info.LatestStampFile) {
+    $StampContent = Get-Content $h8info.LatestStampFile
+    if ((Get-Date $latestData[0].date) -eq (Get-Date $stampContent)) {
+        # If the timestamp in the file on disk matches the latest.json,
+        # It means we're already up to date, no changes
+        $AlreadyUpToDate = $true
+    }
+}
+
+if ($AlreadyUpToDate) {
+    "No new imagery available. Exiting"
+    break;
+}
 
 "Grabbing image dated $latest"
 
@@ -29,7 +69,7 @@ $images = @{
 # Get the images
 $imagesToProcess = @()
 foreach ($image in $images.GetEnumerator()) {
-    $imgOutPath = "$env:userprofile\h8_$($image.name).png"
+    $imgOutPath = "$workingDirectory\h8_$($image.name).png"
     Invoke-WebRequest -Uri $image.Value -OutFile $imgOutPath
     $imagesToProcess += $imgOutPath
 }
@@ -50,7 +90,7 @@ foreach ($image in $imagesToProcess) {
     
     $xCoord = [int]($image.Split("_")[-2]) * [int]$wallpaperSize.Width
     $yCoord = [int]($image.Split("_")[-1]).Split(".")[0] * [int]$wallpaperSize.Height
-    "$image`: $xCoord $yCoord"
+    "Placing $image at $xCoord,$yCoord"
     $g.DrawImage($imgbmp, [System.Drawing.Rectangle]::new($xCoord,$yCoord,$wallpaperSize.Width, $wallpaperSize.Height))
 }
 
@@ -76,3 +116,6 @@ if (Test-Path $wallpaperFile) {
 
 # Clean up $WallpaperSize as we don't need it any more
 $wallpaperSize = $null
+
+# Write out the timestamp file so we don't update unnecessarily
+(Get-Date $latestData[0].date).ToString() | Set-Content $h8info.LatestStampFile
